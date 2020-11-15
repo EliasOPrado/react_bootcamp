3874bb975b51425a2798c45377f4cd60fa4d825d
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-11-15 18:19:44 +0000

    added callback setState

--- state/state-issue/src/ScoreKeeper.js
+++ state/state-issue/src/ScoreKeeper.js
@@ -5,6 +5,7 @@ class ScoreKeeper extends Component {
         super(props);
         this.state = {score: 0};
         this.singleKill = this.singleKill.bind(this);
+        this.tripleKill = this.tripleKill.bind(this);
     }
 
 singleKill(){
@@ -18,12 +19,36 @@ singleKill(){
     // Not a good idea
     this.setState({score: this.state.score + 1});
 }
+
+// tripleKill(){
+//     // will give only on as same as simgle kill
+//     this.setState({score: this.state.score + 3})
+// }
+
+// tripleKill(){
+//     // with a callback WORKING FINE
+//     this.setState(st => {return{score: st.score + 1}});
+//     this.setState(st => {return{score: st.score + 1}});
+//     this.setState(st => {return{score: st.score + 1}});
+// }
+
+incrementScore(curState){
+    return{score: curState.score + 1};
+}
+
+tripleKill() {
+    this.setState(this.incrementScore);
+    this.setState(this.incrementScore);
+    this.setState(this.incrementScore);
+}
+
 render(){
 
     return(
         <div>
             <h1>Score is: {this.state.score}</h1>
             <button onClick={this.singleKill}>Single Kill!</button>
+            <button onClick={this.tripleKill}>Triple Kill</button>
         </div>
     )
 }
