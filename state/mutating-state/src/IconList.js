c9af56599514fe7adc86cb1df1149f066b1b809b
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-11-17 15:49:31 +0000

    added better function to IconList component

--- state/mutating-state/src/IconList.js
+++ state/mutating-state/src/IconList.js
@@ -21,17 +21,22 @@ class IconList extends Component {
 
     constructor(props){
         super(props);
+        // default state
         this.state = { icons: ["bone"]};
+        // add function to constructor
         this.addIcon = this.addIcon.bind(this);
     };
 
     // addIcon(){
-    //     // get icon randonly from option's state
+    //     // 1- get icon randonly from option's state
     //     let idx = Math.floor(Math.random() * this.props.options.length);
-    //     // add icon on props with a random value
+    //     // 2- add icon on props with a random value
     //     let newIcon = this.props.options[idx];
+    //     // 3- goes inco constructor and get the icons value
     //     let icons = this.state.icons;
+    //     // 4- adds the randonaised icon from props into state...
     //     icons.push(newIcon);
+    //     // 5- adds the icons as this.state in constructor.
     //     this.state({icons:icons})
     // }
 
