34d909009e8944d541a4014e1d3f6b72a8c5aa55
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-11-17 00:08:04 +0000

    added IconList component

--- state/mutating-state/src/App.js
+++ state/mutating-state/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import IconList from './IconList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Elias</h1>
+      <IconList />
     </div>
   );
 }
