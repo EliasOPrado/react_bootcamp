300d44343d31209d5c7d342a87c3b44d32c44c70
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-11-17 16:38:26 +0000

    initial commit

---
+++ state/lottery/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Ball from './Ball';
+
+function App() {
+  return (
+    <div className="App">
+      <Ball />
+        
+    </div>
+  );
+}
+
+export default App;
