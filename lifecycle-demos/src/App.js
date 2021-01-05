641c25cb3cdf92c0dab8bf9a278b074650dcd307
EliasOPrado <eliaspradoprofessional@outlook.com>
2021-01-05 06:48:56 -0300

    added lifecycle

---
+++ lifecycle-demos/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ZenQuote from './ZenQuote';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test</h1>
+      <ZenQuote />
+    </div>
+  );
+}
+
+export default App;
