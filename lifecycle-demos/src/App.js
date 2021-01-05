9e889b4c3aebd36e50f08e33d064ebdac760256d
EliasOPrado <eliaspradoprofessional@outlook.com>
2021-01-05 07:48:55 -0300

    added componentDidUnmont

--- lifecycle-demos/src/App.js
+++ lifecycle-demos/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import ZenQuote from './ZenQuote';
+import GithubUserInfo from './GithubUserInfo';
 
 function App() {
   return (
     <div className="App">
-      <ZenQuote />
+      <GithubUserInfo username='facebook' />
+      <GithubUserInfo username='EliasOPrado' />
     </div>
   );
 }
