fa5baf089d0875d1a70e289198519ff629d75ae5
Oliveira Prado <eliaspradoprofessional@outlook.com>
2021-03-25 00:47:05 -0300

    created components

--- devpleno_projects/01-contador-voltas/src/App.js
+++ devpleno_projects/01-contador-voltas/src/App.js
@@ -1,41 +1,64 @@
+import React, {useState, useEffect} from 'react'
+import './Style.css'
+import MostrarVoltas from './MostrarVoltas'
+import MostrarTempo from './MostrarTempo'
+import Button from './Button'
 
-const MostrarVoltas = (props) => {
-  return (
-    <p>
-    {props.voltas}
-      <br/>
-      Voltas
-      </p>
-  )
-}
-
-const MostrarTempo = (props) => {
-  return (
-    <p>
-    {props.tempo}
-      <br/>
-      Tempo medio por volta
-      </p>
-  )
-}
-
-const Button = (props) => <button>{props.text}</button>
 
 function App() {
+
+  const [numVoltas, setNumVoltas] = useState(0)
+  const [running, setRunning] = useState(false)
+  const [tempo, setTempo] = useState(0)
+
+  useEffect(() => {
+    let timer = null
+    if(running){
+    timer = setInterval(() => {
+      setTempo(old => old + 1)
+    }, 1000)
+  }
+  return () => {
+    if(timer){
+      clearInterval(timer)
+    }
+  }
+    // the sqr bracket shows that the code doesnt have dependences.
+  }, [running])
+
+  const toggleRunning = () => {
+    setRunning(!running)
+  }
+
+  const increment = () => {
+    setNumVoltas(numVoltas + 1)
+  }
+  
+  const decrement = () => {
+    if(numVoltas > 0){
+    setNumVoltas(numVoltas - 1)
+    }
+  }
+
+  const reset = () => {
+    setNumVoltas(0)
+    setTempo(0)
+  }
   return (
     <div>
       
-    <MostrarVoltas voltas='16'/>
+    <MostrarVoltas voltas={numVoltas}/>
 
       
-      <Button text = '+' />
-      <Button text = '-' />
+      <Button text = '+' onClick={increment}/>
+      <Button text = '-' onClick={decrement}/>
       <br/><br/>
-
-      <MostrarTempo tempo='01:35'/>
-
-      <Button text = 'Iniciar' />
-      <Button text = 'Reiniciar' />
+      {
+        numVoltas > 0 &&
+      <MostrarTempo tempo={Math.round(tempo / numVoltas)}/>
+      }
+      <Button onClick={toggleRunning} text = 'Iniciar' />
+      <Button onClick={reset} text = 'Reiniciar' />
     </div>
   );
 }
