74bff1ba46d7ec98ed8a5cf4b12990ee7d5543ad
Oliveira Prado <eliaspradoprofessional@outlook.com>
2021-03-31 23:59:21 -0300

    initial commit

--- devpleno_projects/minhas-series/src/App.js
+++ devpleno_projects/minhas-series/src/App.js
@@ -1,33 +1,42 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from "react";
+import Header from './Header'
 import {
-  Navbar,
-  NavbarBrand,
-  Collapse,
-  Nav,
-  NavItem,
-  NavLink,
-  NavbarToggler
- } from 'reactstrap'
-function App() {
-  const [open, setOpen] = useState(false)
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+import axios from 'axios'
 
-  const toggle = () => {
-    setOpen(!open)
-  }
+const Home = () => {
+
   return (
-    <div >
-     <Navbar color='light' light expand='md'>
-      <NavbarBrand>Minhas Series</NavbarBrand>
-      <NavbarToggler onClick={toggle}/>
-      <Collapse isOpen={open} navbar>
-        <Nav className='ml-auto' navbar>
-          <NavItem>
-            <NavLink href='/'>Generos</NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
-      </Navbar>
-    </div>
+    <h1>Home</h1>
+  )
+}
+
+const Generos = () => {
+
+  return (
+    <h1>Generos</h1>
+  )
+}
+
+
+function App() {
+  const [data, setData] = useState({})
+  useEffect(() => {
+    axios.get('/api/').then(res => {
+      setData(res.data)
+    })
+  }, [])
+  return (
+    <Router>
+    <div>
+      <Header/>
+      <Route path='/' exact component={Home} />
+      <Route path='/generos' component={Generos} />
+      <pre>{JSON.stringify(data)}</pre>
+      </div>
+    </Router>
   );
 }
 
