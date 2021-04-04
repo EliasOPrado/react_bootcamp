5768594bdef11cc0d7cc2f477363bb37953d6b46
Oliveira Prado <eliaspradoprofessional@outlook.com>
2021-04-04 01:58:29 -0300

    added useEffect conditional

--- code-exercises-yt/exercises-3-hooks/src/App.js
+++ code-exercises-yt/exercises-3-hooks/src/App.js
@@ -3,6 +3,7 @@ import ClassCount from './components/ClassCount'
 import HookCounter from './components/HookCounter'
 import HookCounter2 from './components/HookCounter2'
 import HookCounter4 from './components/HookCounter4'
+import HookCounterOne from './components/HookCounterOne'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <HookCounter/>
       <HookCounter2/>
       <HookCounter4/>
+      <HookCounterOne/>
     </div>
   );
 }
