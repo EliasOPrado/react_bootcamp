fddb06cd7615b1cdf04753bf9f90bc8bff85060a
Oliveira Prado <eliaspradoprofessional@outlook.com>
2021-04-04 02:11:14 -0300

    added changes to useEffect to run only once

--- code-exercises-yt/exercises-3-hooks/src/App.js
+++ code-exercises-yt/exercises-3-hooks/src/App.js
@@ -4,15 +4,16 @@ import HookCounter from './components/HookCounter'
 import HookCounter2 from './components/HookCounter2'
 import HookCounter4 from './components/HookCounter4'
 import HookCounterOne from './components/HookCounterOne'
-
+import HookMouse from './components/HookMouse'
 function App() {
   return (
     <div className="App">
-      <ClassCount/>
+     {/*<ClassCount/>
       <HookCounter/>
       <HookCounter2/>
-      <HookCounter4/>
-      <HookCounterOne/>
+      <HookCounter4/> 
+      <HookCounterOne/> */}
+      <HookMouse />
     </div>
   );
 }
